512e73cfa4454790c21abcea2318061c540a54fb
M Yousuf Soomro <muhammad.yousuf1996@gmail.com>
2020-02-03 14:08:05 +0500

    team pages updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,3 +48,22 @@ $(document).ready(function () {
   $('.dataTables_length').addClass('bs-select');
 });
   
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 5000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+function profileNotAvailable(){
+  Toast.fire({
+    icon: 'info',
+    title: 'Profile currently not available!'
+  })
+}
