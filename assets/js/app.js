d01f872bfbf8fa1a6a837208a273ff2c0e30003b
M Yousuf Soomro <muhammad.yousuf1996@gmail.com>
2021-03-25 17:53:23 +0500

    Added Download Company Profile Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,8 @@
-function contactUsForm() {
-  const form_name = window.document.getElementById("form-name").value;
-  const form_email = window.document.getElementById("form-email").value;
-  const form_Subject = window.document.getElementById("form-Subject").value;
-  const form_text = window.document.getElementById("form-text").value;
+window.document.getElementById('last_update').innerHTML = "Last updated 25-Mar-2021";
 
-  const newObj = {
-    form_name,
-    form_email,
-    form_Subject,
-    form_text
-  };
-  console.log(newObj);
-
-  window.open(
-    "https://docs.google.com/forms/d/e/1FAIpQLScmvr6oEwjBTPoTD-5c6dW8YUTEpjLgSgCxx0gFUVCYiOXmfQ/viewform?usp=pp_url&entry.1065942813=" +
-      form_email +
-      "&entry.334795048=" +
-      form_Subject +
-      "&entry.983970514=" +
-      form_name +
-      "&entry.85124918=" +
-      form_text,
-    "_blank"
-  );
+function downloadProfile(){
+  window.location.href = "http://bit.ly/TECprofile";
 }
-
-window.document.getElementById('last_update').innerHTML = "Last updated 22-Mar-2021";
 
 $(document).ready(function () {
   $('#dtBasicExample').DataTable({
